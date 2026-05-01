492faa9425f1b394326898b05c4df8706149f1ec
itzLilix <112267705+itzLilix@users.noreply.github.com>
2026-05-02 03:40:51 +0400

    Refactor UserListSort constants: add SortSessionsCount

--- DTOs/queries.go
+++ DTOs/queries.go
@@ -3,10 +3,11 @@ package dtos
 type UserListSort string
 
 const (
-	SortRating       UserListSort = "rating"
-	SortRecent       UserListSort = "recent"
-	SortFollowedAt   UserListSort = "followedAt"
-	SortReviewsCount UserListSort = "reviews"
+	SortRating        UserListSort = "rating"
+	SortRecent        UserListSort = "recent"
+	SortFollowedAt    UserListSort = "followedAt"
+	SortReviewsCount  UserListSort = "reviews"
+	SortSessionsCount UserListSort = "sessions"
 )
 
 type SortOrder string
