706dc23c01feee575ccc2d6fd4e82fe5330729fa
itzLilix <112267705+itzLilix@users.noreply.github.com>
2026-05-24 11:46:20 +0400

    feat: stats added to UserBrief

--- DTOs/userBrief.go
+++ DTOs/userBrief.go
@@ -5,4 +5,7 @@ type UserBrief struct {
 	Username    string  `json:"username"`
 	DisplayName string  `json:"displayName"`
 	AvatarURL   *string `json:"avatarUrl,omitempty"`
+	Rating float64 `json:"rating"`
+	Hosted int `json:"hosted"`
+	Played int `json:"played"`
 }
